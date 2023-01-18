df684f655fb92cfc9e78ef2ce245caa8ffd06507
mukultayal135 <mukultayal135@gmail.com>
2023-01-18 13:00:50 +0530

    chore: add test file for gameScore

---
+++ gameScore.test.js
@@ -0,0 +1,10 @@
+const { getScore } = require('./gameScore.js');
+
+describe('Array Utilities', () => {
+    it('should give the total score of the game', () => {
+        expect(() => {
+            getScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
+        }).toBe(90);
+    });
+
+});
